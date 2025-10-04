380464f90c2d755f3b149d865f148a2a98b4843d
na50r <shanrosan@gmail.com>
2025-10-04 12:34:06 +0200

    store job data & setup only translation for sanity check

---
+++ only_translate.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash -l
+#SBATCH --partition teaching
+#SBATCH --time=0:35:0
+#SBATCH --ntasks=1
+#SBATCH --mem=16GB
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus=1
+#SBATCH --output=translate_only_out.out
+
+module load gpu
+module load mamba
+source activate atmt
+export XLA_FLAGS=--xla_gpu_cuda_data_dir=$CONDA_PREFIX/pkgs/cuda-toolkit
+
+# TRANSLATE
+python translate.py \
+    --cuda \
+    --input ~/shares/atomt.pilot.s3it.uzh/cz-en/data/raw/test.cz \
+    --src-tokenizer cz-en/tokenizers/cz-bpe-8000.model \
+    --tgt-tokenizer cz-en/tokenizers/en-bpe-8000.model \
+    --checkpoint-path cz-en/checkpoints/checkpoint_best.pt \
+    --output cz-en/output/only_translate_output.txt \
+    --bleu \
+    --reference ~/shares/atomt.pilot.s3it.uzh/cz-en/data/raw/test.en \
+    --max-len 300
