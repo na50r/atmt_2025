7ed4a5957253476b63157cd57e4ab9f09f9508b9
na50r <shanrosan@gmail.com>
2025-10-07 09:54:22 +0200

    add tl shell scripts

---
+++ cz-en.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash -l
+#SBATCH --partition teaching
+#SBATCH --time=24:0:0
+#SBATCH --ntasks=1
+#SBATCH --mem=16GB
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus=1
+#SBATCH --output=cz-en.out
+
+module load gpu
+module load mamba
+source activate atmt
+export XLA_FLAGS=--xla_gpu_cuda_data_dir=$CONDA_PREFIX/pkgs/cuda-toolkit
+
+# TRANSLATE
+python translate.py \
+    --cuda \
+    --input ~/shares/atomt.pilot.s3it.uzh/cz-en/data/raw/test.cz \
+    --src-tokenizer cz-en/tokenizers/cz-bpe-8000.model \
+    --tgt-tokenizer cz-en/tokenizers/en-bpe-8000.model \
+    --checkpoint-path cz-en/checkpoints/checkpoint_best.pt \
+    --output cz-en.en \
+    --bleu \
+    --reference ~/shares/atomt.pilot.s3it.uzh/cz-en/data/raw/test.en \
+    --max-len 300
---
+++ sk-en.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash -l
+#SBATCH --partition teaching
+#SBATCH --time=24:0:0
+#SBATCH --ntasks=1
+#SBATCH --mem=16GB
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus=1
+#SBATCH --output=sk-en.out
+
+module load gpu
+module load mamba
+source activate atmt
+export XLA_FLAGS=--xla_gpu_cuda_data_dir=$CONDA_PREFIX/pkgs/cuda-toolkit
+
+# TRANSLATE
+python translate.py \
+    --cuda \
+    --input ep-sk-en-test/test.sk \
+    --src-tokenizer cz-en/tokenizers/cz-bpe-8000.model \
+    --tgt-tokenizer cz-en/tokenizers/en-bpe-8000.model \
+    --checkpoint-path cz-en/checkpoints/checkpoint_best.pt \
+    --output sk-en.en \
+    --bleu \
+    --reference ep-sk-en-test/test.en \
+    --max-len 300
