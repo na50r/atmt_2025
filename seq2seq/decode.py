1dacec27ecdf7f4a6e15e4904c13eec11f5c9e83
na50r <shanrosan@gmail.com>
2025-11-01 15:16:22 +0100

    account for tensor location (cpu/gpu)

--- seq2seq/decode.py
+++ seq2seq/decode.py
@@ -6,21 +6,23 @@ from seq2seq.models import Seq2SeqModel
 def beam_search(prediction, k=10):
     batch_size, seq_length, vocab_size = prediction.shape
     log_prob, indices = prediction[:, 0, :].topk(k, sorted=True)
-    indices = indices.unsqueeze(-1)
+    indices = indices.unsqueeze(-1).to(prediction.device)  # ensure same device
     for n1 in range(1, seq_length):
         log_prob_temp = log_prob.unsqueeze(-1) + \
             prediction[:, n1, :].unsqueeze(1).repeat(1, k, 1)
         log_prob, index_temp = log_prob_temp.view(
             batch_size, -1).topk(k, sorted=True)
-        idx_begin = index_temp // vocab_size  # retrieve index of start sequence
-        idx_concat = index_temp % vocab_size  # retrieve index of new token
-        new_indices = torch.zeros((batch_size, k, n1+1), dtype=torch.int64)
+        # move to same device
+        idx_begin = (index_temp // vocab_size).to(indices.device)
+        idx_concat = index_temp % vocab_size
+
+        new_indices = torch.zeros(
+            (batch_size, k, n1+1), dtype=torch.int64, device=prediction.device)
         for n2 in range(batch_size):
             new_indices[n2, :, :-1] = indices[n2][idx_begin[n2]]
             new_indices[n2, :, -1] = idx_concat[n2]
         indices = new_indices
     return indices
-
 
 def decode_beam_search(model: Seq2SeqModel, src_tokens: torch.Tensor, src_pad_mask: torch.Tensor, max_out_len: int,
            tgt_tokenizer: spm.SentencePieceProcessor, args, device: torch.device, beam_width: int = 5):
