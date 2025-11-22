ac0c10d93ef0d0287d37fa3260a30018db67ac07
na50r <shanrosan@gmail.com>
2025-11-22 15:12:18 +0100

    add new base training

--- a5_train_base.sh
+++ a5_train_base.sh
@@ -12,36 +12,38 @@ module load mamba
 source activate atmt
 export XLA_FLAGS=--xla_gpu_cuda_data_dir=$CONDA_PREFIX/pkgs/cuda-toolkit
 
-mkdir cz-en-a3-task1-base
+OUTDIR=cz-en-a5-base
+SEED=512
+mkdir $OUTDIR
 
 # PREPARE DATA
 python preprocess.py \
     --source-lang cz \
     --target-lang en \
     --raw-data ~/shares/cz-en/data/raw \
-    --dest-dir ./cz-en-a3-task1-base/data/prepared \
-    --model-dir ./cz-en-a3-task1-base/tokenizers \
+    --dest-dir ./$OUTDIR/data/prepared \
+    --model-dir ./$OUTDIR/tokenizers \
     --test-prefix test \
     --train-prefix train \
     --valid-prefix valid \
     --src-vocab-size 8000 \
     --tgt-vocab-size 8000 \
-    --src-model ./cz-en-a3-task1-base/tokenizers/cz-bpe-8000.model \
-    --tgt-model ./cz-en-a3-task1-base/tokenizers/en-bpe-8000.model
+    --src-model ./$OUTDIR/tokenizers/cz-bpe-8000.model \
+    --tgt-model ./$OUTDIR/tokenizers/en-bpe-8000.model
 
 # TRAIN
 python train.py \
     --cuda \
-    --data cz-en-a3-task1-base/data/prepared/ \
-    --src-tokenizer cz-en-a3-task1-base/tokenizers/cz-bpe-8000.model \
-    --tgt-tokenizer cz-en-a3-task1-base/tokenizers/en-bpe-8000.model \
+    --data $OUTDIR/data/prepared/ \
+    --src-tokenizer $OUTDIR/tokenizers/cz-bpe-8000.model \
+    --tgt-tokenizer $OUTDIR/tokenizers/en-bpe-8000.model \
     --source-lang cz \
     --target-lang en \
     --batch-size 64 \
     --arch transformer \
     --max-epoch 7 \
-    --log-file cz-en-a3-task1-base/logs/train.log \
-    --save-dir cz-en-a3-task1-base/checkpoints/ \
+    --log-file $OUTDIR/logs/train.log \
+    --save-dir $OUTDIR/checkpoints/ \
     --ignore-checkpoints \
     --encoder-dropout 0.1 \
     --decoder-dropout 0.1 \
@@ -52,17 +54,18 @@ python train.py \
     --max-seq-len 300 \
     --n-encoder-layers 3 \
     --n-decoder-layers 3 \
-    --seed 666
+    --seed $SEED
 
 # TRANSLATE
 python translate.py \
     --cuda \
     --input ~/shares/cz-en/data/raw/test.cz \
-    --src-tokenizer cz-en-a3-task1-base/tokenizers/cz-bpe-8000.model \
-    --tgt-tokenizer cz-en-a3-task1-base/tokenizers/en-bpe-8000.model \
-    --checkpoint-path cz-en-a3-task1-base/checkpoints/checkpoint_best.pt \
-    --output cz-en-a3-task1-base/output.txt \
+    --src-tokenizer $OUTDIR/tokenizers/cz-bpe-8000.model \
+    --tgt-tokenizer $OUTDIR/tokenizers/en-bpe-8000.model \
+    --checkpoint-path $OUTDIR/checkpoints/checkpoint_best.pt \
+    --output $OUTDIR/output.txt \
     --max-len 300 \
     --bleu \
     --reference ~/shares/cz-en/data/raw/test.en \
-    --seed 666
+    --seed $SEED \
+    --beam-size 1 
