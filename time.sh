58ec4a6b7a2e9d7ec87fad1d9ceb5e3133de94d1
na50r <shanrosan@gmail.com>
2025-11-23 13:34:46 +0100

    fix

--- time.sh
+++ time.sh
@@ -5,7 +5,7 @@ echo "Translation times extracted:" >> "$out"
 date_stamp=$(date)
 echo "Date: $date_stamp" >> "$out" 
 curr=$(git branch --show-current)
-echo "Branch: $curr" > "$out" 
+echo "Branch: $curr" >> "$out" 
 for i in 1 3 5; do
     file="a5_test_beam_${i}.out"
     time_d=$(grep "Translation" "$file" | grep -Po "\S+ seconds")
