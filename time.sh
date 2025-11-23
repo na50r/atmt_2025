17b51b864f053505c86ff03671651e762d6de7a7
na50r <shanrosan@gmail.com>
2025-11-23 13:33:58 +0100

    improve bash script

--- time.sh
+++ time.sh
@@ -8,7 +8,8 @@ curr=$(git branch --show-current)
 echo "Branch: $curr" > "$out" 
 for i in 1 3 5; do
     file="a5_test_beam_${i}.out"
-    grep "Translation" "$file" >> "$out"
+    time_d=$(grep "Translation" "$file" | grep -Po "\S+ seconds")
+    echo $file $time_d >> "$out"
 done
 echo "" >> "$out"
 
