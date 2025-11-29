92e36e63f07cc20106bf53aed5befeb65c235ba7
na50r <shanrosan@gmail.com>
2025-11-29 16:27:56 +0100

    update times script

--- a5_task4/times.sh
+++ a5_task4/times.sh
@@ -4,7 +4,7 @@ date_stamp=$(date)
 echo "Date: $date_stamp" >> $out 
 curr=$(git branch --show-current)
 echo "Branch: $curr" >> $out
-for i in 1 3 5; do
+for i in 3 5 10; do
     file="a5_task4_beam${i}.out"
     time_d=$(grep "Translation" $file | grep -Po "\S+ seconds")
     bleu=$(grep 'BLEU' $file)
