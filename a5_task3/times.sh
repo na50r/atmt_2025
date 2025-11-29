bdd5e825e9ef70d4169934b1a9afa918474a812c
na50r <shanrosan@gmail.com>
2025-11-29 11:55:28 +0100

    fix times script

--- a5_task3/times.sh
+++ a5_task3/times.sh
@@ -5,7 +5,7 @@ echo "Date: $date_stamp" >> "$out"
 curr=$(git branch --show-current)
 echo "Branch: $curr" >> "$out" 
 for i in 1 3 5; do
-    file="a5_test_beam_${i}.out"
+    file="a5_task3_beam${i}.out"
     time_d=$(grep "Translation" "$file" | grep -Po "\S+ seconds")
     echo $file $time_d >> "$out"
 done
