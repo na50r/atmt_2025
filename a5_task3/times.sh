03fb3fccf5bdb687b06de72c3f46814ddd0e36ce
na50r <shanrosan@gmail.com>
2025-11-29 11:57:55 +0100

    fix consistency

--- a5_task3/times.sh
+++ a5_task3/times.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 out="times.txt"
 date_stamp=$(date)
-echo "Date: $date_stamp" >> "$out" 
+echo "Date: $date_stamp" >> $out 
 curr=$(git branch --show-current)
-echo "Branch: $curr" >> "$out" 
+echo "Branch: $curr" >> $out
 for i in 1 3 5; do
     file="a5_task3_beam${i}.out"
-    time_d=$(grep "Translation" "$file" | grep -Po "\S+ seconds")
-    echo $file $time_d >> "$out"
+    time_d=$(grep "Translation" $file | grep -Po "\S+ seconds")
+    bleu=$(grep 'BLEU' $file)
+    echo $file $time_d >> $out
+    echo $file $bleu >> $out
 done
-echo "" >> "$out"
+echo "" >> $out
