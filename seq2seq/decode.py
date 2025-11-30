e4fe93de6f1b4b8d95be1abf1f1fb85e746b52ed
na50r <shanrosan@gmail.com>
2025-11-30 12:27:45 +0100

    increase rpl from 0.02 -> 0.3

--- seq2seq/decode.py
+++ seq2seq/decode.py
@@ -98,7 +98,7 @@ def beam_search_decode(model: Seq2SeqModel, src_tokens: torch.Tensor, src_pad_ma
             # Relative scaling with log probs not ideal, we use real probs instead
             scaled_score = math.exp(score)
             scaled_best = math.exp(curr_best)
-            if scaled_score <= (scaled_best * 0.02):
+            if scaled_score <= (scaled_best * 0.3):
                 print(f'DEBUG: Remove {scaled_score:.2f} <= {scaled_best} * RPL (RPL=0.02)')
             else:
                 updated_beams.append(b)
