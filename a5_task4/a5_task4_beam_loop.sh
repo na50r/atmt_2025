4b5b26abd6dadc2c0c5eb66b41533a87cd47c343
na50r <shanrosan@gmail.com>
2025-12-02 11:52:29 +0100

    fix outdir

--- a5_task4/a5_task4_beam_loop.sh
+++ a5_task4/a5_task4_beam_loop.sh
@@ -13,7 +13,8 @@ source activate atmt
 export XLA_FLAGS=--xla_gpu_cuda_data_dir=$CONDA_PREFIX/pkgs/cuda-toolkit
 
 MODEL_DIR=~/shares/groups/turing/a5/cz-en-a5-base
-OUT_DIR=~/shares/groups/turing/a5/a5_task4
+OUT_DIR=~/shares/groups/turing/a5/a5_task4_loop
+mkdir $OUT_DIR
 SCRIPTS=~/shares/groups/turing/a5
 SEED=512
 cat ~/shares/cz-en/data/raw/test.cz | head -n 100 > test.cz
@@ -28,7 +29,7 @@ for threshold in 0.5 1 1.5 2 2.5 3 3.5 4; do
         --src-tokenizer $MODEL_DIR/tokenizers/cz-bpe-8000.model \
         --tgt-tokenizer $MODEL_DIR/tokenizers/en-bpe-8000.model \
         --checkpoint-path $MODEL_DIR/checkpoints/checkpoint_best.pt \
-        --output $OUT_DIR/a5_task4_beam3.txt \
+        --output "$OUT_DIR/a5_task4_beam${beam_size}_threshold${threshold}.txt" \
         --max-len 300 \
         --bleu \
         --reference test.en \
