5b117c468dcc00abf3969fca8e4876ed9040cbb2
na50r <shanrosan@gmail.com>
2025-12-07 12:18:27 +0100

    Update code for translations

--- a5_task4/a5_task4_beam15.sh
+++ a5_task4/a5_task4_beam15.sh
@@ -7,8 +7,7 @@
 #SBATCH --gpus=1
 #SBATCH --output=a5_task4_beam15.out
 
-module load gpu
-module load mamba
+module load miniforge3
 source activate atmt
 export XLA_FLAGS=--xla_gpu_cuda_data_dir=$CONDA_PREFIX/pkgs/cuda-toolkit
 
--- a5_task4/start.sh
+++ a5_task4/start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+OUT_DIR=~/shares/groups/turing/a5/a5_task4/translations
+rm -rf $OUT_DIR
+mkdir $OUT_DIR
 for i in 3 5 10 15 20; do
     file="a5_task4_beam${i}.sh"
     sbatch $file
